dac7da24ebcb06e6113988b07ad9aa0f40228df0
Unique Dismuke <uniquedismuke@uniques-air.mynetworksettings.com>
2023-11-15 12:49:13 -0500

    download and add font to folder

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,19 @@
+import "./LandingPage.scss"
+import { Link } from "react-router-dom"
+
+const LandingPage = () => {
+    return (
+        <>
+        <header>
+            <p>U Pick Your Fit</p>
+        </header>
+
+        <section>
+            <p></p>
+            <Link to=""><button>Begin Styling</button></Link>
+        </section>
+        </>
+    )
+}
+
+export default LandingPage;
