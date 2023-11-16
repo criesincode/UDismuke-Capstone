2fc2fe7faf6151963753cb63fd8ee113af9ba3bf
Unique Dismuke <uniquedismuke@uniques-air.mynetworksettings.com>
2023-11-16 10:20:59 -0500

    render outfit and outfit route

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,14 @@
 import "./Main.scss"
+import { Link } from "react-router-dom";
 
 const Main = () => {
     return (
         <main>
         <div className="land-text__container"><p className="land-text">Wish you could put together outfits from different online stores from one app? Then U PICK YOUR FIT is the app for U! Browse the inventory of your favorite online stores, build your fit and purchase your items all in one place!</p></div>
+        <Link to="/outfit">
         <button>Begin Styling</button>
+        </Link>
+       
     </main>
     )
 }
---
+++ src/pages/CuratedFit/CuratedFit.js
@@ -0,0 +1,16 @@
+import "./CuratedFit.scss"
+
+const CuratedFit = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+
+
+
+
+
+
+export default CuratedFit;
