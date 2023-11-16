3b8fb7762e0c70222cb8a7a5b48880cf40dbbf03
Unique Dismuke <uniquedismuke@uniques-air.mynetworksettings.com>
2023-11-16 10:51:24 -0500

    create skeleton for outfit page

--- src/pages/CuratedFit/CuratedFit.js
+++ src/pages/CuratedFit/CuratedFit.js
@@ -3,6 +3,38 @@ import "./CuratedFit.scss"
 const CuratedFit = () => {
     return (
         <>
+<body>
+    <h1>URL Selector</h1>
+
+    <form id="urlForm">
+        <label for="urlInput">URL:</label>
+        <input type="text" id="urlInput" name="urlInput" required/>
+
+        <label for="dropdown1">Dropdown 1:</label>
+        <select id="dropdown1" name="dropdown1">
+            <option value="option1">Option 1</option>
+            <option value="option2">Option 2</option>
+           
+        </select>
+
+        <label for="dropdown2">Dropdown 2:</label>
+        <select id="dropdown2" name="dropdown2">
+            <option value="optionA">Option A</option>
+            <option value="optionB">Option B</option>
+           
+        </select>
+
+        <label for="dropdown3">Dropdown 3:</label>
+        <select id="dropdown3" name="dropdown3">
+            <option value="optionX">Option X</option>
+            <option value="optionY">Option Y</option>
+            
+        </select>
+
+        <button type="button" onclick="submitForm()">Submit</button>
+    </form>
+</body>
+
         </>
     )
 }
