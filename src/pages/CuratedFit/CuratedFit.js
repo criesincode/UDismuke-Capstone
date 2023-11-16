2e803ae03cd80b8a0b64b61297f328d1abf5762e
Unique Dismuke <uniquedismuke@uniques-air.mynetworksettings.com>
2023-11-16 11:04:35 -0500

    define drop down menus

--- src/pages/CuratedFit/CuratedFit.js
+++ src/pages/CuratedFit/CuratedFit.js
@@ -4,27 +4,35 @@ const CuratedFit = () => {
     return (
         <>
             <body>
+                <nav>
+                    <div className="nav-item__1">
+                        <p className="nav-text__1">U Pick Your Fit</p>
+                    </div>
+                    <div className="nav-item__2">
+                        <p className="nav-text__2"> Clear Selections</p>
+                    </div>
+                </nav>
                 <h1>URL Selector</h1>
 
-                <form id="urlForm">
+                <form id="uForm">
                     <label for="urlInput">URL:</label>
                     <input type="text" id="urlInput" name="urlInput" required />
 
-                    <label for="dropdown1">Dropdown 1:</label>
+                    <label for="dropdown1">Choose category: Top, Bottoms or Shoes</label>
                     <select id="dropdown1" name="dropdown1">
                         <option value="option1">Option 1</option>
                         <option value="option2">Option 2</option>
 
                     </select>
 
-                    <label for="dropdown2">Dropdown 2:</label>
+                    <label for="dropdown2">Style type:</label>
                     <select id="dropdown2" name="dropdown2">
                         <option value="optionA">Option A</option>
                         <option value="optionB">Option B</option>
 
                     </select>
 
-                    <label for="dropdown3">Dropdown 3:</label>
+                    <label for="dropdown3">Body type:</label>
                     <select id="dropdown3" name="dropdown3">
                         <option value="optionX">Option X</option>
                         <option value="optionY">Option Y</option>
