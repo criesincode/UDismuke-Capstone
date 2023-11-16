d2368ff2444a4eae4a47b45cd8c1b68c3e2fb2e4
Unique Dismuke <uniquedismuke@uniques-air.mynetworksettings.com>
2023-11-16 12:23:59 -0500

    figure out page structure

--- src/pages/CuratedFit/CuratedFit.js
+++ src/pages/CuratedFit/CuratedFit.js
@@ -3,45 +3,50 @@ import "./CuratedFit.scss"
 const CuratedFit = () => {
     return (
         <>
-            <body>
-                <nav>
-                    <div className="nav-item__1">
-                        <p className="nav-text__1">U Pick Your Fit</p>
+            <nav>
+                <div className="nav-item__1">
+                    <p className="nav-text__1">U Pick Your Fit</p>
+                </div>
+                <div className="nav-item__2">
+                    <p className="nav-text__2"> Clear Selections</p>
+                </div>
+            </nav>
+
+            <section>
+                <h1>Build Your Fit</h1>
+
+                <form className="form">
+                    <div className="form-input__container">
+                        <div className="form-url">
+                            <label for="urlInput">URL:</label>
+                            <input type="text" id="urlInput" name="urlInput" required /></div>
+                        <div className="form-category">
+                            <label for="dropdown1">Choose category: Top, Bottoms or Shoes</label>
+                            <select id="dropdown1" name="dropdown1">
+                                <option value="option1">Option 1</option>
+                                <option value="option2">Option 2</option>
+                            </select>
+                        </div>
+
+                        <div className="form-style">
+                            <label for="dropdown2">Style type:</label>
+                            <select id="dropdown2" name="dropdown2">
+                                <option value="optionA">Option A</option>
+                                <option value="optionB">Option B</option>
+                            </select>
+                        </div>
+
+                        <div className="form-body">
+                            <label for="dropdown3">Body type:</label>
+                            <select id="dropdown3" name="dropdown3">
+                                <option value="optionX">Option X</option>
+                                <option value="optionY">Option Y</option>
+                            </select>
+                        </div>
+                        <button type="button" onclick="submitForm()">Submit</button>
                     </div>
-                    <div className="nav-item__2">
-                        <p className="nav-text__2"> Clear Selections</p>
-                    </div>
-                </nav>
-                <h1>URL Selector</h1>
-
-                <form id="uForm">
-                    <label for="urlInput">URL:</label>
-                    <input type="text" id="urlInput" name="urlInput" required />
-
-                    <label for="dropdown1">Choose category: Top, Bottoms or Shoes</label>
-                    <select id="dropdown1" name="dropdown1">
-                        <option value="option1">Option 1</option>
-                        <option value="option2">Option 2</option>
-
-                    </select>
-
-                    <label for="dropdown2">Style type:</label>
-                    <select id="dropdown2" name="dropdown2">
-                        <option value="optionA">Option A</option>
-                        <option value="optionB">Option B</option>
-
-                    </select>
-
-                    <label for="dropdown3">Body type:</label>
-                    <select id="dropdown3" name="dropdown3">
-                        <option value="optionX">Option X</option>
-                        <option value="optionY">Option Y</option>
-
-                    </select>
-
-                    <button type="button" onclick="submitForm()">Submit</button>
                 </form>
-            </body>
+            </section>
 
         </>
     )
