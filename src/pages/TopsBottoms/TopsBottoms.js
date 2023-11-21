88d4410de8495b9f909fd73e48585f42aefe4bb3
Unique Dismuke <uniquedismuke@uniques-air.mynetworksettings.com>
2023-11-21 12:27:10 -0500

    see if this works

--- src/pages/TopsBottoms/TopsBottoms.js
+++ src/pages/TopsBottoms/TopsBottoms.js
@@ -1,6 +1,26 @@
+import React, { useState } from 'react';
 import "./TopsBottoms.scss"
 
 const TopsBottoms = () => {
+    const [selectedBodyType, setSelectedBodyType] = useState('');
+
+    const submitForm = () => {
+        // Your form submission logic goes here
+    }
+
+    const renderImages = () => {
+        if (selectedBodyType === 'optionY') { // Plus
+            return (
+                <>
+                    <img src="" alt="Plus Image 1" />
+                    <img src="path/to/jacket3.webp" alt="Plus Image 2" />
+                    <img src="path/to/curatedtop.webp" alt="Plus Image 3" />
+                </>
+            );
+        }
+        // Add more conditions for other body types if needed
+        return null; // No images to display for other body types
+    }
     return (
         <>
             <nav>
@@ -25,7 +45,7 @@ const TopsBottoms = () => {
                             <select id="dropdown1" name="dropdown1">
                                 <option value="option1">Tops</option>
                                 <option value="option2">Bottoms</option>
-                                <option value="option2">Shoes</option>
+                                <option value="option3">Shoes</option>
                             </select>
                         </div>
 
@@ -34,23 +54,41 @@ const TopsBottoms = () => {
                             <select id="dropdown2" name="dropdown2">
                                 <option value="optionA">Crop Top</option>
                                 <option value="optionB">Jackets</option>
-                                <option value="optionB">Sweater</option>
-                                <option value="optionB">Button Up</option>
+                                <option value="optionC">Sweater</option>
+                                <option value="optionD">Button Up</option>
                             </select>
                         </div>
 
                         <div className="form-body">
                             <label className="dropdown3">Body type:</label>
                             <select id="dropdown3" name="dropdown3">
-                                <option value="optionX">Slim</option>
-                                <option value="optionY">Tall</option>
-                                <option value="optionB">Plus</option>
-                                <option value="optionB">Petite</option>
+                                <option value="optionW">Slim</option>
+                                <option value="optionX">Tall</option>
+                                <option value="optionY">Plus</option>
+                                <option value="optionZ">Petite</option>
                             </select>
                         </div>
                         <button type="button" onclick="submitForm()">Submit</button>
                     </div>
                 </form>
+                <div className="form-body">
+                <label className="dropdown3">Body type:</label>
+                <select
+                    id="dropdown3"
+                    name="dropdown3"
+                    value={selectedBodyType}
+                    onChange={(e) => setSelectedBodyType(e.target.value)}
+                >
+                    <option value="optionW">Slim</option>
+                    <option value="optionX">Tall</option>
+                    <option value="optionY">Plus</option>
+                    <option value="optionZ">Petite</option>
+                </select>
+            </div>
+
+            {renderImages()}
+
+            <button type="button" onClick={submitForm}>Submit</button>
             </section>
 
         </>
