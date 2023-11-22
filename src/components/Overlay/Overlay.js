3d40cacaeff0ed225a716d258cb0d689427d3971
Unique Dismuke <uniquedismuke@uniques-air.mynetworksettings.com>
2023-11-22 13:56:21 -0500

    change color scheme

--- src/components/Overlay/Overlay.js
+++ src/components/Overlay/Overlay.js
@@ -1,12 +1,18 @@
 import Fashion from "../../assets/images/istockphoto-1307568521-1024x1024-removebg.png"
 import React from 'react';
+import { Link } from "react-router-dom";
 import './Overlay.scss';
 
 const Overlay = () => {
   return (
+    <>
     <div className="overlay-container">
       <img src={Fashion} alt="Overlay Image" />
     </div>
+    <Link to="/Tops">
+        <button>Begin Styling</button>
+        </Link>
+    </>
   );
 }
 
