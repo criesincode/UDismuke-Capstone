e4d7aadda5e4fa8bb652f789e322d166abd745e8
Unique Dismuke <uniquedismuke@Uniques-MacBook-Air.local>
2023-11-22 22:07:38 -0500

    edit main info

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,7 +5,7 @@ import Overlay from "../Overlay/Overlay";
 const Main = () => {
     return (
         <main>
-        <div className="land-text__container"><p className="land-text">Wish you could put together outfits from different online stores from one app? Then U PICK YOUR FIT is the app for U! Browse the inventory of your favorite online stores, build your fit and purchase your items all in one place!</p></div>
+        <div className="land-text__container"><p className="land-text">Wish you could put together outfits from different online stores in one app? Then U PICK YOUR FIT is the app for U! Browse the inventory of your favorite online stores, build your fit and purchase your items all in one place!</p></div>
        <Overlay />
     </main>
     )
